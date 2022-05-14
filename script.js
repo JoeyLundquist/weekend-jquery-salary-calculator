2f13821c383c51f324f5519506c799804a5857d3
Joey Lundquist <joeylundquist91@gmail.com>
2022-05-13 20:15:47 -0500

    displayOnEmployeeTable function displays on DOM

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function addEmployeeToTable (firstName, lastName, id, jobTitle, annualSalary) {
     //Not let the object get pushed if missing information
     if(firstName == null || firstName === '' || lastName == null || lastName === '' || id == null || id === '' || 
     jobTitle == null || jobTitle === '' || annualSalary == null || annualSalary === '') {
+        alert('Missing Fields')
         return false;
     }
     //Pushing new employeeObject to employeeObjectArray
@@ -27,12 +28,38 @@ function addEmployeeToTable (firstName, lastName, id, jobTitle, annualSalary) {
 }
 
 function onSubmit () {
+    //Setting getter values to variable
     let employeeFirstName = $('#employeeFirstName').val();
     let employeeLastName = $('#employeeLastName').val();
     let employeeIdNumber = $('#employeeIdNumber').val();
     let employeeJobTitle = $('#employeeJobTitle').val();
     let employeeAnnualSalary = $('#employeeAnnualSalary').val();
-
+    //adding inputs to the addEmployeeToTable function
     addEmployeeToTable(employeeFirstName, employeeLastName, employeeIdNumber, employeeJobTitle, employeeAnnualSalary);
+    //emptying fields after button push
+    $('#employeeFirstName').val('');
+    $('#employeeLastName').val('');
+    $('#employeeIdNumber').val('');
+    $('#employeeJobTitle').val('');
+    $('#employeeAnnualSalary').val('');
+    displayOnEmployeeTable();
 }
 
+function displayOnEmployeeTable() {
+    let el = $('#employeeMonthlyCostTable');
+    //Empty table before looping over array to re-insert employees.
+    el.empty();
+    // looping through employeeObjectArray to append to DOM
+    for(let employee of employeeObjectArray){
+        el.append(`
+            <tr>
+                <td>${employee.firstName}</td>
+                <td>${employee.lastName}</td>
+                <td>${employee.idNumber}</td>
+                <td>${employee.jobTitle}</td>
+                <td>${employee.annualSalary}</td>
+                <td>
+                    <button class="deleteEmployeeFromTable">Delete</button>
+                </td>`)
+    }
+}
