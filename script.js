a34fdefe48fddabfe9f0ddc98db7f87dfe254378
Joey Lundquist <joeylundquist91@gmail.com>
2022-05-14 09:21:12 -0500

    Finished onDelete which deletes row and employeeObject from array and recalculates monthly cost

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(readyNow);
 
 function readyNow() {
     $(document).on('click', '#submitEmployeeInfoToTable', onSubmit);
-    //$(document).on('click', '#deleteEmployeeFromTable', onDelete);
+    $(document).on('click', '.deleteEmployeeFromTable', onDelete);
 }
 
 const employeeObjectArray = [];
@@ -17,11 +17,11 @@ function addEmployeeToTable (firstName, lastName, id, jobTitle, annualSalary) {
         annualSalary: annualSalary
     }
     //Not let the object get pushed if missing information
-    if(firstName == null || firstName === '' || lastName == null || lastName === '' || id == null || id === '' || 
+    /*if(firstName == null || firstName === '' || lastName == null || lastName === '' || id == null || id === '' || 
     jobTitle == null || jobTitle === '' || annualSalary == null || annualSalary === '') {
         alert('Missing Fields')
         return false;
-    }
+    }*/
     //Pushing new employeeObject to employeeObjectArray
     employeeObjectArray.push(newEmployeeObject);
     return employeeObjectArray;
@@ -46,25 +46,34 @@ function onSubmit () {
     displayOnEmployeeTable();
     // Calculate monthly cost
     calculateMonthlyCost();
-    //Display monthly cost
+
 }
 
+
 function displayOnEmployeeTable() {
+    //setting variable for my .data() function
+let deleteButtonCounter = 1;
     let el = $('#employeeMonthlyCostTable');
     //Empty table before looping over array to re-insert employees.
     el.empty();
+    //Variable to keep my delete button id
+    
     // looping through employeeObjectArray to append to DOM
     for(let employee of employeeObjectArray){
         el.append(`
             <tr>
+                <td>${deleteButtonCounter}.</td>
                 <td>${employee.firstName}</td>
                 <td>${employee.lastName}</td>
                 <td>${employee.idNumber}</td>
                 <td>${employee.jobTitle}</td>
-                <td>${employee.annualSalary}</td>
+                <td>$${employee.annualSalary}</td>
                 <td>
-                    <button class="deleteEmployeeFromTable">Delete</button>
-                </td>`)
+                    <button class = "deleteEmployeeFromTable">Delete</button>
+                </td>
+            </tr>
+            <div class="deleteCounter></div>"`);    
+        deleteButtonCounter ++;
     }
 }
 
@@ -83,4 +92,12 @@ function calculateMonthlyCost() {
     monthlyCostOutput.empty();
     //append monthly cost to dom
     monthlyCostOutput.append(monthlyCost);
+}
+
+function onDelete() {
+    let deleteStartNumber = $(this).parent().siblings().first();
+    console.log($(this).parent().siblings().first());
+    employeeObjectArray.splice(deleteStartNumber, 1);
+    displayOnEmployeeTable();
+    calculateMonthlyCost();
 }
